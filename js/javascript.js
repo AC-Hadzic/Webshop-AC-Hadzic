e970f2312a788d1dd0df2496bd96f51fe0f181fd
AC-Hadzic <alen_cezar.hadzic@outlook.com>
2024-02-11 18:26:24 +0100

    loader & dark mode

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,4 @@
+// iFrame for Header/Footer
 document.addEventListener("DOMContentLoaded", function () 
     {
         function adjustIframeHeight(iframe) {
@@ -15,6 +16,7 @@ document.addEventListener("DOMContentLoaded", function ()
     }
 );
 
+// Clear shop page
 function delete_all()
 {
     var naslov = document.getElementById("naslov");
@@ -29,11 +31,14 @@ function delete_all()
     prazno.style.display = "block";
 }
 
+// Reload page
 function refresh()
 {
     location.reload();
 }
 
+
+// Toggle Card payment display on/off
 function pop_me()
 {
     const oznaceno = document.getElementById('card');
@@ -50,6 +55,7 @@ function pop_me()
     }
 }
 
+// Toggle filters in product page
 function showToggle()
 {
     const aside = document.getElementById('aside');
@@ -64,3 +70,94 @@ function showToggle()
         aside.style.display = 'none';
     }
 }
+
+// Definitions for themes
+const light = "/images/icons/light_icon.svg";
+const night = "/images/icons/night_icon.svg";
+const icon = document.getElementById("theme_icon");
+const body = document.body;  
+const header = document.getElementById("header_iframe");
+const footer = document.getElementById("footer_iframe");
+
+// Toggle light/dark theme & save user preference in cookies
+function theme_toggle() 
+{
+    if (body.classList == "")
+    {
+        body.classList = "light_mode";
+        document.cookie = "theme=light_mode; path=/";
+
+        // Check if header & footer exists or else many null errors :c
+        if (header && header.contentWindow && header.contentWindow.document.body &&
+            footer && footer.contentWindow && footer.contentWindow.document.body)
+        {
+            header.contentWindow.document.body.classList = "light_mode";
+            footer.contentWindow.document.body.classList = "light_mode";
+        }
+        icon.src = night;
+    }
+
+    else if (body.classList == "light_mode")
+    {
+        body.classList = "";
+        document.cookie = "theme=dark_mode; path=/";
+
+        if (header && header.contentWindow && header.contentWindow.document.body && 
+            footer && footer.contentWindow && footer.contentWindow.document.body)
+        {
+            header.contentWindow.document.body.classList = "";
+            footer.contentWindow.document.body.classList = "";
+        }
+        icon.src = light;
+    }
+} 
+
+// Load theme from cookies on page load
+window.onload = function()
+{
+    if (document.cookie == "theme=light_mode")
+        {
+            body.classList = "light_mode";
+
+            // Check if header & footer exists or else many null errors :c
+            if (header && header.contentWindow && header.contentWindow.document.body && 
+                footer && footer.contentWindow && footer.contentWindow.document.body)
+            {
+                header.contentWindow.document.body.classList = "light_mode";
+                footer.contentWindow.document.body.classList = "light_mode";
+
+                icon.src = night;
+            }
+        }
+
+    else if (document.cookie == "theme=dark_mode")
+    {
+        body.classList = ""
+    
+        // Check if header & footer exists or else many null errors :c
+        if (header && header.contentWindow && header.contentWindow.document.body && 
+        footer && footer.contentWindow && footer.contentWindow.document.body)
+        {
+            header.contentWindow.document.body.classList = "";
+            footer.contentWindow.document.body.classList = "";
+
+            icon.src = light;
+        }
+    }    
+}
+
+// Page Loader
+var aaa = document.getElementById("loader");
+
+function loader() {
+    aaa.style.opacity = '0';
+    setTimeout();
+
+}
+
+setTimeout(() => {
+    if (aaa)
+    {
+        aaa.remove(); 
+    }
+  }, 400)
